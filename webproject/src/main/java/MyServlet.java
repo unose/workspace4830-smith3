2115b572fdf53b22da212bf20b4206d07d683cb4
Myoungkyu Song <myoungkyu@unomaha.edu>
2024-02-19 22:22:27 -0600

    Added a print.

--- webproject/src/main/java/MyServlet.java
+++ webproject/src/main/java/MyServlet.java
@@ -25,8 +25,8 @@ public class MyServlet extends HttpServlet {
     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
     */
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      // TODO Auto-generated method stub
-      response.getWriter().append("Hello World!!!");
+      response.setContentType("text/html");
+      response.getWriter().println("Hello World!!!" + "<br>" + "Hello World!!!");
    }
 
    /**
